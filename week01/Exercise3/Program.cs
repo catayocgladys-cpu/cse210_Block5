99baea8717a8b4561d13ac97c5e0537078a22fcc
catayocgladys-cpu <gcatayoc@byupathway.edu>
2026-09-12 03:22:24 -0600

    Done with Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Guess My Number game the computer picks a magic number, and then the user tries to guess it. After each guess, the computer tells the user to guess "higher" or "lower" until they guess the magic number.
+       string playAgain = "yes";
+
+        while (playAgain == "yes")
+        {
+            //Ask user to give the range of the magic number
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101);
+
+            int guess = -1;
+            int guessCount = 0;
+
+            Console.WriteLine("Guess the magic number!");
+
+            //Ask the user to guess the magic number until they get it right
+            // this is the loop that will keep asking the user for their guess until they guess the magic number
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                guessCount++;
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            Console.WriteLine($"You made {guessCount} guesses.");
+
+            Console.Write("Do you want to play again? ");
+            playAgain = Console.ReadLine();
+        }
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
